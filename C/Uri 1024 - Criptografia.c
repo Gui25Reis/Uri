e4cfbe1428283e030e7742b591b38566ffd263f0
Gui Reis <gui.sreis25@gmail.com>
2021-01-05 23:51:46 -0300

    #04 Novo ex
    Add novo exercício.

---
+++ C/Uri 1024 - Criptografia.c
@@ -0,0 +1,37 @@
+#include <stdio.h>                              // Biblioteca padrão de entrada e saída
+#include <stdlib.h>
+#include <string.h>
+
+
+// char passo_01(char txt, int len){
+//     int i;
+//     for (i = 0; i < len ; i++) {
+//         printf("ola\n");
+//     }
+// }
+
+
+int main() {                                    // Função main():
+    int i;
+    const char *c[100];
+
+    scanf("%c", c);
+
+    // passo_01(c, strlen(c));
+
+    // printf('%s \n', c);
+    // printf("Letra: %3d \n\n", c[0]);
+
+    // for (i = 32; i < 127; i++) {
+    //     printf("%d %c  \n", i, i);
+    // }
+
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+// ASCII - [32 : 126] %c
